df6ac00c5d2eeea1f2e1aa15f0bdbd1ffad7f172
trungsliver <buiductrung.cva@gmail.com>
2025-02-08 14:20:16 +0700

    first commit

---
+++ Lesson1/lesson1.py
@@ -0,0 +1,9 @@
+# Data Types - Kiểu dữ liệu
+    # String - chuỗi ký tự / xâu ký tự
+name = "Dũng"
+    # Int (integer): số nguyên
+age = 2
+    # Float (số thực): số thập phân
+score = 4.5
+    # Boolean - True/False Đúng/Sai
+die = False
