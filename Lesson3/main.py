33c6ae8955156e70ef0468f5861fb1476fc984d4
trungsliver <buiductrung.cva@gmail.com>
2025-02-22 15:16:09 +0700

    Lesson 3

--- Lesson3/main.py
+++ Lesson3/main.py
@@ -8,3 +8,15 @@ stu1.introduce()
 stu1.display_info()
 stu1.edit_name()
 stu1.edit_age(2)
+
+# Đề bài:
+# Tạo class Animal gồm các thuộc tính: tên, loài
+# Viết 2 phương thức cho class Animal
+
+# Tạo class Dog kế thừa từ class Animal và có thêm thuộc tính: giống
+# Viết 1 phương thức kế thừa từ class Animal (có sửa đổi)
+# Viết 1 phương thức mới cho class Dog
+
+# Yêu cầu:
+# - Tạo class ở file oop.py
+# - Viết chương trình test tại file main.py
